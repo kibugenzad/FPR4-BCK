1ac56b1aa5fffda133aaab08534a57363733283a
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:31:29 +0000

    bbis-microservices/access-donor/models/blood-usage.js: update

--- bbis-microservices/access-donor/models/blood-usage.js
+++ bbis-microservices/access-donor/models/blood-usage.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Main BloodUsage schema
-const BloodUsageSchema = new Schema({
-  // ID linking to the BloodDonation schema
-  donation: { type: Schema.Types.ObjectId, ref: 'BloodDonation', required: true },
-
-  // Requester Information
-  requester: {
-    name: { type: String, required: true },
-    position: { type: String, required: true },
+const BloodUsageSchema = new Schema(
+  {
+    available: { type: Boolean, default: true },
+    donation: {
+      type: Schema.Types.ObjectId,
+      ref: "BloodDonation",
+      required: true,
+    },
+    requester: {
+      name: { type: String, required: true },
+      position: { type: String, required: true },
+    },
+    hospital: { type: Schema.Types.ObjectId, ref: "Hospital", required: true },
+    isActive: { type: Boolean, default: true },
+    usageDate: { type: Date, required: true },
+    reason: { type: String, required: true },
   },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
 
-  // ID linking to the Hospital schema
-  hospital: { type: Schema.Types.ObjectId, ref: 'Hospital', required: true },
-
-  // Additional Fields
-  isActive: { type: Boolean, default: true },
-  usageDate: { type: Date, required: true },
-  reason: { type: String, required: true },
-  
-}, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
-});
-
-module.exports = mongoose.model('BloodUsage', BloodUsageSchema);
+module.exports = mongoose.model("BloodUsage", BloodUsageSchema);
