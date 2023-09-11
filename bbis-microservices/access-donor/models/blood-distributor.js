dd2623438b5396bc068e05a3d2dfde8d5fd27d8f
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:21:04 +0000

    bbis-microservices/access-donor/models/blood-distributor.js: update

--- bbis-microservices/access-donor/models/blood-distributor.js
+++ bbis-microservices/access-donor/models/blood-distributor.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Address sub-schema
-const addressSchema = new Schema({
-  province: { type: String, required: true },
-  district: { type: String, required: true },
-  sector: { type: String, required: true },
-  cell: { type: String, required: true },
-  village: { type: String, required: true },
-  lat: { type: Number, required: true },
-  long: { type: Number, required: true },
-}, {
-  _id: false // This line ensures that no additional ID is created for this sub-document
-});
+const addressSchema = new Schema(
+  {
+    province: { type: String, required: true },
+    district: { type: String, required: true },
+    sector: { type: String, required: true },
+    cell: { type: String, required: true },
+    village: { type: String, required: true },
+    lat: { type: Number, required: true },
+    long: { type: Number, required: true },
+  },
+  {
+    _id: false, // This line ensures that no additional ID is created for this sub-document
+  }
+);
 
 // Main BloodDistributor schema
-const BloodDistributorSchema = new Schema({
-  // Distributor Name
-  name: { type: String, required: true },
+const BloodDistributorSchema = new Schema(
+  {
+    available: { type: Boolean, default: true },
+    name: { type: String, required: true },
+    address: { type: addressSchema, required: true },
+    contactNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
 
-  // Address Object
-  address: { type: addressSchema, required: true },
-
-  // Contact Information
-  contactNumber: { type: String, required: true },
-  email: { type: String, required: true },
-
-  // Additional Fields
-  isActive: { type: Boolean, default: true },
-}, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
-});
-
-module.exports = mongoose.model('BloodDistributor', BloodDistributorSchema);
+module.exports = mongoose.model("BloodDistributor", BloodDistributorSchema);
