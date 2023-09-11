a3836e346144394b062ea8a739bdc89b0007acd2
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:31:29 +0000

    bbis-microservices/access-donor/models/hospital.js: update

--- bbis-microservices/access-donor/models/hospital.js
+++ bbis-microservices/access-donor/models/hospital.js
@@ -1,3 +1,39 @@
-// should have name
-// should have address
-// etc components needed for a hospital like type etc
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const HospitalSchema = new Schema(
+  {
+    available: {
+      type: Boolean,
+      default: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    address: {
+      province: String,
+      district: String,
+      sector: String,
+      cell: String,
+      village: String,
+    },
+    type: {
+      type: String,
+      enum: ["Public", "Private", "Military", "Research"],
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Hospital = mongoose.model("Hospital", HospitalSchema);
+
+module.exports = Hospital;
