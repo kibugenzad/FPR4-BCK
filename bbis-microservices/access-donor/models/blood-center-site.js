cc57a3d4e9fbc86ac5c6b2f259a49d31b1f6e9d0
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 19:22:23 +0000

    bbis-microservices/access-donor/models/blood-center-site.js: update

--- bbis-microservices/access-donor/models/blood-center-site.js
+++ bbis-microservices/access-donor/models/blood-center-site.js
@@ -7,35 +7,52 @@ const addressSchema = new Schema({
   sector: { type: String, required: true },
   cell: { type: String, required: true },
   village: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
 }, {
   _id: false // This line ensures that no additional ID is created for this sub-document
 });
 
 const BloodCenterSiteSchema = new Schema({
-  // ID linking to the Center schema
-  center: { type: Schema.Types.ObjectId, ref: 'Center', required: true },
-
-  // ID linking to the Blood Donation Committee schema
-  committeeLeader: { type: Schema.Types.ObjectId, ref: 'Committee', required: true },
-
-  // Site Name
-  name: { type: String, required: true },
-
-  // Site Code
-  code: { type: String, required: true, unique: true },
-
-  // Address Object
-  address: { type: addressSchema, required: true },
-
-  // Additional Fields
-  contactNumber: { type: String, required: true },
-  email: { type: String, required: true },
-  isActive: { type: Boolean, default: true },
-  
-  // Availability Status
-  available: { type: Boolean, default: true },
+  center: { 
+    type: Schema.Types.ObjectId,
+    ref: 'Center',
+    required: true
+  },
+  committeeLeader: { 
+    type: Schema.Types.ObjectId,
+    ref: 'Committee',
+    required: true 
+  },
+  name: { 
+    type: String,
+    required: true 
+  },
+  code: { 
+    type: String,
+    required: true,
+    unique: true 
+  },
+  address: { 
+    type: addressSchema,
+    required: true 
+  },
+  contactNumber: { 
+    type: String,
+  },
+  email: { 
+    type: String,
+  },
+  isActive: { 
+    type: Boolean,
+    default: true 
+  },
+  available: { 
+    type: Boolean,
+    default: true
+  },
 }, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
+  timestamps: true
 });
 
 module.exports = mongoose.model('BloodCenterSite', BloodCenterSiteSchema);
