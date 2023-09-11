47e2da6efba57d0b014ed62673dd963a310dd152
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:31:29 +0000

    bbis-microservices/access-donor/models/pre-donation-assessment.js: update

--- bbis-microservices/access-donor/models/pre-donation-assessment.js
+++ bbis-microservices/access-donor/models/pre-donation-assessment.js
@@ -1,45 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Main PreDonationAssessment schema
-const PreDonationAssessmentSchema = new Schema({
-  // Weight in kilograms
-  weight: { type: Number, required: true },
-  
-  // Temperature in Celsius
-  temperature: { type: Number, required: true },
-
-  // Blood Pressure (systolic and diastolic)
-  bloodPressure: {
-    systolic: { type: Number, required: true },
-    diastolic: { type: Number, required: true }
+const PreDonationAssessmentSchema = new Schema(
+  {
+    available: {
+      type: Boolean,
+      default: true,
+    },
+    weight: { type: Number, required: true },
+    temperature: { type: Number, required: true },
+    bloodPressure: {
+      systolic: { type: Number, required: true },
+      diastolic: { type: Number, required: true },
+    },
+    pulseRate: { type: Number, required: true },
+    hemoglobinLevel: { type: Number, required: true },
+    healthQuestionnaire: {
+      recentIllness: { type: Boolean, required: true },
+      recentMedication: { type: Boolean, required: true },
+      recentSurgery: { type: Boolean, required: true },
+      // to be continued
+    },
+    isEligible: { type: Boolean, default: false },
+    remarks: { type: String },
+    assessedBy: { type: Schema.Types.ObjectId, ref: "Account", required: true },
   },
-  
-  // Pulse rate in beats per minute
-  pulseRate: { type: Number, required: true },
-  
-  // Hemoglobin level in g/dL
-  hemoglobinLevel: { type: Number, required: true },
-  
-  // Health questionnaire results (example)
-  healthQuestionnaire: {
-    recentIllness: { type: Boolean, required: true },
-    recentMedication: { type: Boolean, required: true },
-    recentSurgery: { type: Boolean, required: true },
-    // Add more as needed
-  },
-  
-  // Eligibility status, computed based on the above information
-  isEligible: { type: Boolean, default: false },
-  
-  // Any other remarks or considerations
-  remarks: { type: String },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
 
-  // The ID of the assessor or health professional
-  assessedBy: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
-
-}, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
-});
-
-module.exports = mongoose.model('PreDonationAssessment', PreDonationAssessmentSchema);
+module.exports = mongoose.model(
+  "PreDonationAssessment",
+  PreDonationAssessmentSchema
+);
