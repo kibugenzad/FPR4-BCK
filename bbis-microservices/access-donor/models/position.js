758099fe17cf21b0f8e261732f0f2ece4a4d0f0f
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:31:29 +0000

    bbis-microservices/access-donor/models/position.js: update

--- bbis-microservices/access-donor/models/position.js
+++ bbis-microservices/access-donor/models/position.js
@@ -7,33 +7,33 @@ const Schema = mongoose.Schema;
 
 // Create Position schema
 const PositionSchema = new Schema(
-    {
-        available: {
-            type: Boolean,
-            default: true
-        },
-        name: {
-            type: String,
-            required: [true, "name field is required"],
-            unique: true,
-            trim: true
-        },
-        description: {
-            type: String,
-            trim: true
-        },
-        department: {
-            type: Schema.Types.ObjectId,
-            ref: "Department",
-            required: [true, "department field is required"]
-        },
+  {
+    available: {
+      type: Boolean,
+      default: true,
     },
-    { 
-        timestamps: true,
-        index: {
-            name: 1,
-        }  
-    }
+    name: {
+      type: String,
+      required: [true, "name field is required"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    department: {
+      type: Schema.Types.ObjectId,
+      ref: "Department",
+      required: [true, "department field is required"],
+    },
+  },
+  {
+    timestamps: true,
+    index: {
+      name: 1,
+    },
+  }
 );
 
 // Create the model based on the schema
