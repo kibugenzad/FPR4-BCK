4c0359b21c8a8402990b8df432130afe1d2fb153
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:21:04 +0000

    bbis-microservices/access-donor/models/blood-request.js: update

--- bbis-microservices/access-donor/models/blood-request.js
+++ bbis-microservices/access-donor/models/blood-request.js
@@ -1,45 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Packages sub-schema
-const packagesSchema = new Schema({
-  bloodGroup: { type: String, required: true, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'] },
-  numberOfPackages: { type: Number, required: true },
-}, {
-  _id: false
-});
-
-// Approval sub-schema
-const approvalSchema = new Schema({
-  account: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
-  level: { type: String, required: true, enum: ['Low', 'Medium', 'High'] },
-  status: { type: String, required: true, enum: ['Pending', 'Approved', 'Rejected'] },
-}, {
-  _id: false
-});
+const packagesSchema = new Schema(
+  {
+    bloodGroup: {
+      type: String,
+      required: true,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    },
+    numberOfPackages: { type: Number, required: true },
+  },
+  {
+    _id: false,
+  }
+);
 
 // Main BloodRequest schema
-const BloodRequestSchema = new Schema({
-  // ID linking to the Distributor or Hospital schema
-  linkedEntity: {
-    distributor: { type: Schema.Types.ObjectId, ref: 'Distributor', required: false },
-    hospital: { type: Schema.Types.ObjectId, ref: 'Hospital', required: false },
+const BloodRequestSchema = new Schema(
+  {
+    available: { type: Boolean, default: true },
+    linkedEntity: {
+      distributor: {
+        type: Schema.Types.ObjectId,
+        ref: "Distributor",
+        required: false,
+      },
+      hospital: {
+        type: Schema.Types.ObjectId,
+        ref: "Hospital",
+        required: false,
+      },
+    },
+    description: { type: String, required: true },
+    packages: { type: [packagesSchema], required: true },
+    approvals: [
+      {
+        account: {
+          type: Schema.Types.ObjectId,
+          ref: "Account",
+          required: true,
+        },
+        level: {
+          type: Number,
+          required: true,
+        },
+        approved: {
+          type: Boolean,
+          default: false,
+        },
+        approvedAt: Date,
+      },
+    ],
+    isActive: { type: Boolean, default: true },
   },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
 
-  // Description
-  description: { type: String, required: true },
-
-  // Number of Packages Grouped by Type
-  packages: { type: [packagesSchema], required: true },
-
-  // Approval Sequence
-  approvals: { type: [approvalSchema], required: true },
-
-  // Additional Fields
-  isActive: { type: Boolean, default: true },
-  
-}, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
-});
-
-module.exports = mongoose.model('BloodRequest', BloodRequestSchema);
+module.exports = mongoose.model("BloodRequest", BloodRequestSchema);
