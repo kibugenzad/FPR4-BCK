199f1e7b0ab28d93bd53b42821c7a686673f2db1
duwizerak <davy.uwizerak@gmail.com>
2023-09-11 20:21:04 +0000

    bbis-microservices/access-donor/models/blood-donation-commitee.js: update

--- bbis-microservices/access-donor/models/blood-donation-commitee.js
+++ bbis-microservices/access-donor/models/blood-donation-commitee.js
@@ -1,25 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Main BloodDonationCommittee schema
-const BloodDonationCommitteeSchema = new Schema({
-  // Committee Name
-  name: { type: String, required: true },
-
-  // Contact Information
-  contact: {
-    phone: { type: String, required: true },
+const BloodDonationCommitteeSchema = new Schema(
+  {
+    available: { type: Boolean, default: true },
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
     email: { type: String, required: true },
+    site: { type: Schema.Types.ObjectId, ref: "Site", required: true },
+    isActive: { type: Boolean, default: true },
   },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
 
-  // ID linking to the Site schema
-  site: { type: Schema.Types.ObjectId, ref: 'Site', required: true },
-
-  // Additional Fields
-  isActive: { type: Boolean, default: true },
-
-}, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
-});
-
-module.exports = mongoose.model('BloodDonationCommittee', BloodDonationCommitteeSchema);
+module.exports = mongoose.model(
+  "BloodDonationCommittee",
+  BloodDonationCommitteeSchema
+);
