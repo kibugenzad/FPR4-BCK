a72ff4df9deee5cd8947a96241ce5ca4fac7c4c6
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 03:50:12 +0000

    bbis-microservices/access-donor/controller/bbis-access.js: update

--- bbis-microservices/access-donor/controller/bbis-access.js
+++ bbis-microservices/access-donor/controller/bbis-access.js
@@ -6,37 +6,43 @@ the services
 
 const Services = require("../services");
 
-
 /*
 Controller
 */
 class BBISAccess {
-    static performAction(action, req, serviceName) {
-        // Validate the service name
-        const Service = Services[serviceName];
-        if (!Service) {
-            throw new Error(`Invalid service name: ${serviceName}`);
-        }
-
-        // Perform the action
-        return Service[action](req);
+  static performAction(action, req, serviceName) {
+    // Validate the service name
+    const Service = Services[serviceName];
+    if (!Service) {
+      throw new Error(`Invalid service name: ${serviceName}`);
     }
 
-    static get(req, serviceName) {
-        return this.performAction("get", req, serviceName);
-    }
+    // Perform the action
+    return Service[action](req);
+  }
 
-    static create(req, serviceName) {
-        return this.performAction("create", req, serviceName);
-    }
+  static get(req, serviceName) {
+    return this.performAction("get", req, serviceName);
+  }
 
-    static update(req, serviceName) {
-        return this.performAction("update", req, serviceName);
-    }
+  static create(req, serviceName) {
+    return this.performAction("create", req, serviceName);
+  }
 
-    static delete(req, serviceName) {
-        return this.performAction("delete", req, serviceName);
+  static update(req, serviceName) {
+    return this.performAction("update", req, serviceName);
+  }
+
+  static delete(req, serviceName) {
+    return this.performAction("delete", req, serviceName);
+  }
+
+  static authenticate(req, serviceName) {
+    if (!req.body.password) {
+      throw new Error("No password provided in the request");
     }
+    return this.performAction("authenticate", req, serviceName);
+  }
 }
 
 module.exports = BBISAccess;
