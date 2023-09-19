36cd063ebe062cce90cd87d58e9200f9298f7719
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 03:50:12 +0000

    bbis-microservices/access-donor/commons/utils/handler.js: update

--- bbis-microservices/access-donor/commons/utils/handler.js
+++ bbis-microservices/access-donor/commons/utils/handler.js
@@ -1,37 +1,57 @@
-const paginate = (req, data) => {
-    const { page, limit } = req.body;
-    const responseData = { data };
-    
-    if (page) {
-        responseData.count = data.length < limit 
-            ? ((page - 1) * limit) + data.length 
-            : "many";
-    }
+const paginate = (page, limit, data) => {
+  const responseData = { data };
 
-    return responseData;
+  if (page) {
+    responseData.count =
+      data.length < limit ? (page - 1) * limit + data.length : "many";
+  }
+
+  return responseData;
 };
 
 const alignDataResponse = (req, data, paginate) => {
-    if (req.body.tag === "count") return { data };
+  const { limit, page, tag } = { ...req.params, ...req.body };
+  if (tag === "count") return { data };
 
-    return req.body.page ? paginate(req, data) : data;
+  return page && limit ? paginate(page, limit, data) : data;
 };
 
 const handler = {
-    handleRequest: async (actionType, req, res, next, serviceName, Controller) => {
-        try {
-            const data = await Controller[actionType](req, serviceName);
-            
-            const responseData = actionType === "get" 
-                ? alignDataResponse(req, data, paginate) 
-                : data;
+  handleRequest: async (
+    actionType,
+    req,
+    res,
+    next,
+    serviceName,
+    Controller
+  ) => {
+    try {
+      const data = await Controller[actionType](req, serviceName);
 
-            res.send(responseData);
+      const responseData =
+        actionType === "get" ? alignDataResponse(req, data, paginate) : data;
 
-        } catch (error) {
-            next(error);
-        }
+      res.send(responseData);
+    } catch (error) {
+      next(error);
     }
+  },
+
+  handleAuthentication: async (
+    actionType,
+    req,
+    res,
+    next,
+    serviceName,
+    Controller
+  ) => {
+    try {
+      const responseData = await Controller[actionType](req, serviceName);
+      res.send(responseData);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
-  
+
 module.exports = handler;
