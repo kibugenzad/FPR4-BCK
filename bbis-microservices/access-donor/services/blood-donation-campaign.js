32e9c6a26fea769b4735f40fc67afbc611d705a4
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 04:07:54 +0000

    bbis-microservices/access-donor/services/blood-donation-campaign.js: update

--- bbis-microservices/access-donor/services/blood-donation-campaign.js
+++ bbis-microservices/access-donor/services/blood-donation-campaign.js
@@ -7,7 +7,7 @@ const {
   processExactQuery,
 } = require("../commons/utils/general-filters");
 
-class Position {
+class BloodDonationCampaign {
   static buildQuery(filters) {
     let query = { available: true }; // enforce availability
     query = filterDates(query, filters);
