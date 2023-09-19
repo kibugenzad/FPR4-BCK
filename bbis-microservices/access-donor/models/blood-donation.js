5b91973e9d572d75113d5137bb7317d1cb35ad76
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 04:36:41 +0000

    bbis-microservices/access-donor/models/blood-donation.js: update

--- bbis-microservices/access-donor/models/blood-donation.js
+++ bbis-microservices/access-donor/models/blood-donation.js
@@ -1,28 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Blood Information sub-schema
-const bloodInfoSchema = new Schema({
-  bloodGroup: { type: String, required: true, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'] },
-  rhFactor: { type: String, required: true, enum: ['Positive', 'Negative'] },
-  volume: { type: Number, required: true },  // Measured in ml
-  tested: { type: Boolean, default: false }, // If the blood has been tested
-  expirationDate: { type: Date, required: true }, // Expiration date for the donated blood
-}, {
-  _id: false // This line ensures that no additional ID is created for this sub-document
-});
+const bloodInfoSchema = new Schema(
+  {
+    bloodGroup: {
+      type: String,
+      required: true,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    },
+    rhFactor: { type: String, required: true, enum: ["Positive", "Negative"] },
+    volume: { type: Number, required: true }, // Measured in ml
+    tested: { type: Boolean, default: false }, // If the blood has been tested
+    expirationDate: { type: Date, required: true }, // Expiration date for the donated blood
+  },
+  {
+    _id: false, // This line ensures that no additional ID is created for this sub-document
+  }
+);
 
 // Main BloodDonation schema
-const BloodDonationSchema = new Schema({
-  available: { type: Boolean, default: true },
-  donor: { type: Schema.Types.ObjectId, ref: 'Donor', required: true },
-  bloodInfo: { type: bloodInfoSchema, required: true },
-  site: { type: Schema.Types.ObjectId, ref: 'Site', required: true },
-  isActive: { type: Boolean, default: true },
-  collectedBy: { type: String, required: true }, // Name or ID of the person who collected the blood
+const BloodDonationSchema = new Schema(
+  {
+    available: { type: Boolean, default: true },
+    donor: { type: Schema.Types.ObjectId, ref: "Donor", required: true },
+    bloodInfo: { type: bloodInfoSchema, required: true },
+    center: { type: Schema.Types.ObjectId, ref: "BloodCenter", required: true },
+    centerSite: {
+      type: Schema.Types.ObjectId,
+      ref: "BloodCenterSite",
+      required: true,
+    },
+    isActive: { type: Boolean, default: true },
+    collectedBy: { type: String, required: true }, // Name or ID of the person who collected the blood
+  },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
 
-}, {
-  timestamps: true  // Enables automatic creation of createdAt and updatedAt fields
-});
-
-module.exports = mongoose.model('BloodDonation', BloodDonationSchema);
+module.exports = mongoose.model("BloodDonation", BloodDonationSchema);
