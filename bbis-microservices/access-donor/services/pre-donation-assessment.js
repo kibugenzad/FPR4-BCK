c0eb177567eb54a5468f9583950d18e75f4abe72
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 04:36:41 +0000

    bbis-microservices/access-donor/services/pre-donation-assessment.js: update

---
+++ bbis-microservices/access-donor/services/pre-donation-assessment.js
@@ -0,0 +1,56 @@
+const Model = require("../models/position");
+const config = require("../commons/config/app-config");
+const {
+  filterDates,
+  filterIds,
+  processArrayQuery,
+  processExactQuery,
+} = require("../commons/utils/general-filters");
+
+class PreDonationAssessment {
+  static buildQuery(filters) {
+    let query = { available: true }; // enforce availability
+    query = filterDates(query, filters);
+    query = filterIds(query, filters);
+
+    const arrayFields = ["department", "service"];
+    const exactFields = ["name"];
+
+    arrayFields.forEach((field) => {
+      processArrayQuery(query, field, filters[field]);
+    });
+
+    exactFields.forEach((field) => {
+      processExactQuery(query, field, filters[field]);
+    });
+
+    return query;
+  }
+
+  static get(req) {
+    const { limit = config.limit, page } = req.body;
+    const query = this.buildQuery(req.body);
+
+    return Model.find(query)
+      .populate({ path: "assessedBy" })
+      .sort({ date: -1 })
+      .limit(limit)
+      .skip(page ? limit * (page - 1) : 0);
+  }
+
+  static create(req) {
+    return Model.create(req.body);
+  }
+
+  static update(req) {
+    const { id } = req.body;
+    return Model.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+  }
+
+  static delete(req) {
+    const { id } = req.body;
+    return Model.remove({ _id: id });
+  }
+}
+
+module.exports = PreDonationAssessment;
