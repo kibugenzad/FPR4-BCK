8c982e7939790f8af66e1f9a184bc5bb4f5f63e0
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 03:50:13 +0000

    bbis-microservices/access-donor/services/service.js: update

--- bbis-microservices/access-donor/services/service.js
+++ bbis-microservices/access-donor/services/service.js
@@ -1,56 +1,55 @@
 const Model = require("../models/service");
 const config = require("../commons/config/app-config");
-const { 
-    filterDates,
-    filterIds,
-    processArrayQuery,
-    processExactQuery,
+const {
+  filterDates,
+  filterIds,
+  processArrayQuery,
+  processExactQuery,
 } = require("../commons/utils/general-filters");
 
-
 class Service {
-    static buildQuery(filters) {
-        let query = { available: true }; // enforce availability
-        query = filterDates(query, filters);
-        query = filterIds(query, filters);
+  static buildQuery(filters) {
+    let query = { available: true }; // enforce availability
+    query = filterDates(query, filters);
+    query = filterIds(query, filters);
 
-        const arrayFields = ["category", "institution", "position"];
-        const exactFields = ["department", "name"];
-        
-        arrayFields.forEach(field => {
-            processArrayQuery(query, field, filters[field]);
-        });
+    const arrayFields = ["category", "institution", "position"];
+    const exactFields = ["department", "name"];
 
-        exactFields.forEach(field => {
-            processExactQuery(query, field, filters[field]);
-        });
+    arrayFields.forEach((field) => {
+      processArrayQuery(query, field, filters[field]);
+    });
 
-        return query;
-    }
+    exactFields.forEach((field) => {
+      processExactQuery(query, field, filters[field]);
+    });
 
-    static get(req) {
-        const { limit = config.limit, page } = req.params;
-        const query = this.buildQuery(req.params);
+    return query;
+  }
 
-        return Model.find(query)
-            .sort({ date: -1 })
-            .limit(limit)
-            .skip(page ? limit * (page - 1) : 0);
-    }
+  static get(req) {
+    const { limit = config.limit, page } = req.body;
+    const query = this.buildQuery(req.body);
 
-    static create(req) {
-        return Model.create(req.body);
-    }
+    return Model.find(query)
+      .sort({ date: -1 })
+      .limit(limit)
+      .skip(page ? limit * (page - 1) : 0);
+  }
 
-    static update(req) {
-        const { id } = req.params;
-        return Model.findByIdAndUpdate({ _id: id }, req.body, { new: true });
-    }
+  static create(req) {
+    return Model.create(req.body);
+  }
 
-    static delete(req) {
-        const { id } = req.params;
-        return Model.remove({ _id: id });
-    }
+  static update(req) {
+    const { id } = req.body;
+    return Model.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+  }
+
+  static delete(req) {
+    const { id } = req.body;
+    return Model.remove({ _id: id });
+  }
 }
 
 module.exports = Service;
