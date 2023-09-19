f5366d5f52fed890a310b8246ef161ccb694eec6
duwizerak <davy.uwizerak@gmail.com>
2023-09-19 04:36:41 +0000

    bbis-microservices/access-donor/services/position.js: update

--- bbis-microservices/access-donor/services/position.js
+++ bbis-microservices/access-donor/services/position.js
@@ -32,6 +32,7 @@ class Position {
     const query = this.buildQuery(req.body);
 
     return Model.find(query)
+      .populate({ path: "position" })
       .sort({ date: -1 })
       .limit(limit)
       .skip(page ? limit * (page - 1) : 0);
