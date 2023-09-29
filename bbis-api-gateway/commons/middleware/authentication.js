90de690d51f3c9ef8dea7478ceb3248ef2a888bb
duwizerak <davy.uwizerak@gmail.com>
2023-09-29 13:23:48 +0000

    bbis-api-gateway/commons/middleware/authentication.js: update

--- bbis-api-gateway/commons/middleware/authentication.js
+++ bbis-api-gateway/commons/middleware/authentication.js
@@ -1,16 +1,18 @@
 const asyncHandler = require("express-async-handler");
-const config = require("../../config");
+const appConfig = require("../config/app-config");
 const jwt = require("jsonwebtoken");
-const Register = require("../../controller/register");
 
 // Function to handle brute-force attack prevention
 const protectBruteForceAuth = (user, reset) => {
   const currentTime = Math.floor(Date.now());
-  const updatedUser = {...user};
-  
+  const updatedUser = { ...user };
+
   // Initialize user tracking if not present
   if (!user.number_auth_try || !user.number_auth_try.maximum_try) {
-    updatedUser.number_auth_try = { maximum_try: 5, maximum_interval: 1000 * 60 * 30 };
+    updatedUser.number_auth_try = {
+      maximum_try: 5,
+      maximum_interval: 1000 * 60 * 30,
+    };
   }
 
   // Reset the counter if the reset flag is set
@@ -34,11 +36,13 @@ const tokenVerify = asyncHandler(async (req, res, next) => {
   // Preprocessing and validation
   const body = preprocessRequestBody(req);
   if (!body) {
-    return res.status(422).send({ success: false, message: "Invalid request body." });
+    return res
+      .status(422)
+      .send({ success: false, message: "Invalid request body." });
   }
 
   // Authentication Logic
-  if (!config.no_token_urls.includes(req.originalUrl)) {
+  if (!appConfig.no_token_urls.includes(req.originalUrl)) {
     return handleTokenAuthentication(req, res, next, body);
   }
 
@@ -47,7 +51,8 @@ const tokenVerify = asyncHandler(async (req, res, next) => {
 });
 
 const preprocessRequestBody = (req) => {
-  let body = req.body && Object.keys(req.body).length > 0 ? req.body : req.fields;
+  let body =
+    req.body && Object.keys(req.body).length > 0 ? req.body : req.fields;
   if (body.amount) body.amount = parseFloat(req.body.amount);
   if (body.contact) body.contact = body.contact.replace("+", "");
   return body;
