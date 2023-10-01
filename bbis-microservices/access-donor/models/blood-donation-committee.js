0c3d64fb0e70a8209923662c942ffcc0761f66cd
duwizerak <davy.uwizerak@gmail.com>
2023-10-01 09:39:15 +0000

    bbis-microservices/access-donor/models/blood-donation-committee.js: update

---
+++ bbis-microservices/access-donor/models/blood-donation-committee.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Main BloodDonationCommittee schema
+const BloodDonationCommitteeSchema = new Schema(
+  {
+    available: { type: Boolean, default: true },
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    center: {
+      type: Schema.Types.ObjectId,
+      ref: "CenterSite",
+      required: true,
+    },
+    centerSite: {
+      type: Schema.Types.ObjectId,
+      ref: "BloodCenterSite",
+      required: true,
+    },
+    isActive: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true, // Enables automatic creation of createdAt and updatedAt fields
+  }
+);
+
+module.exports = mongoose.model(
+  "BloodDonationCommittee",
+  BloodDonationCommitteeSchema
+);
