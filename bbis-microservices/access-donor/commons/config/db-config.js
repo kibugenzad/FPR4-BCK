d3b97c0ad86ed438a9241be0f7c9dabf51316f2e
duwizerak <davy.uwizerak@gmail.com>
2023-10-01 09:39:15 +0000

    bbis-microservices/access-donor/commons/config/db-config.js: update

--- bbis-microservices/access-donor/commons/config/db-config.js
+++ bbis-microservices/access-donor/commons/config/db-config.js
@@ -1,8 +1,12 @@
 //Setting up connection
 //_____________________
+mongoose = require("mongoose");
 
-mongoose.connect("mongodb://huza:HzAmrt12Qpoly@polycluster0-shard-00-00-nnzhu.mongodb.net:27017,polycluster0-shard-00-01-nnzhu.mongodb.net:27017,polycluster0-shard-00-02-nnzhu.mongodb.net:27017/bbis-access?ssl=true&replicaSet=polycluster0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-
+const dbName = "bbis-access-donor";
+mongoose.connect(
+  `mongodb://huza:HzAmrt12Qpoly@polycluster0-shard-00-00-nnzhu.mongodb.net:27017,polycluster0-shard-00-01-nnzhu.mongodb.net:27017,polycluster0-shard-00-02-nnzhu.mongodb.net:27017/${dbName}?ssl=true&replicaSet=polycluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+);
 
 mongoose.Promise = global.Promise;
 module.exports = exports = mongoose;
