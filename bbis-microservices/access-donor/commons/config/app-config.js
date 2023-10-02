7dc25086684951facaa632b88f8474891316008c
duwizerak <davy.uwizerak@gmail.com>
2023-10-02 18:30:21 +0000

    bbis-microservices/access-donor/commons/config/app-config.js: update

--- bbis-microservices/access-donor/commons/config/app-config.js
+++ bbis-microservices/access-donor/commons/config/app-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  limit: 10,
+  limit: 50,
   nonProtectedRoutes: {
     get: ["serviceCategory", "service"],
     post: ["contactUs"],
@@ -7,6 +7,15 @@ module.exports = {
   apiPrefix: "/api",
   secret: "aSZxKA2l8sJnfA4P",
   whitelistedUrls: [
+    "/api/access/authentication/superadmin",
+    "/api/access/authentication/superuser",
+    "/api/access/authentication/account",
+    "/api/access/authentication/user",
+    "/api/access/service",
+    "/api/access/ippis",
+    "/api/access/ippis/user/signup",
+  ],
+  authUrls: [
     "/api/access/authentication/superadmin",
     "/api/access/authentication/superuser",
     "/api/access/authentication/account",
