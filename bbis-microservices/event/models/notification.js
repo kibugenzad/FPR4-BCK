5bb4a65565b2efc99dad5c98dbc2f895cac2cd28
duwizerak <davy.uwizerak@gmail.com>
2023-10-04 18:38:25 +0000

    bbis-microservices/event/models/notification.js: update

--- bbis-microservices/event/models/notification.js
+++ bbis-microservices/event/models/notification.js
@@ -6,19 +6,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Notification Schema & model
-const NotificationSchema = new Schema({
-    account: [{ type: Schema.ObjectId, required: [true, "Account ID required"] }],
+const NotificationSchema = new Schema(
+  {
+    account: [
+      { type: Schema.ObjectId, required: [true, "Account ID required"] },
+    ],
     available: { type: Boolean, default: true },
     body: { type: String, required: [true, "Body field is required"] },
     targetScreen: { type: String },
     title: { type: String },
     user: [{ type: Schema.ObjectId, required: [true, "User ID required"] }],
-    viewed: { type: Boolean, default: true }
-}, {
-    timestamps: true,  // Automatically include createdAt and updatedAt fields
-    index: { account: 1, user: 1 }  // Compound index on "account" and "user"
-});
+    viewed: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true, // Automatically include createdAt and updatedAt fields
+    index: { account: 1, user: 1 }, // Compound index on "account" and "user"
+  }
+);
 
 const Notification = mongoose.model("notification", NotificationSchema);
 module.exports = Notification;
-
