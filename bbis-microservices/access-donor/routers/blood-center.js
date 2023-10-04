4666f36ee1e46f9251c63d6637fa584c95768def
duwizerak <davy.uwizerak@gmail.com>
2023-10-04 18:24:41 +0000

    bbis-microservices/access-donor/routers/blood-center.js: update

--- bbis-microservices/access-donor/routers/blood-center.js
+++ bbis-microservices/access-donor/routers/blood-center.js
@@ -9,27 +9,27 @@ const Controller = require("../controller/bbis-access");
 
 // Set main api URL and service name
 const apiUrl = "/access";
-const routeUrl = `${apiUrl}/position`;
-const serviceName = "Position";
+const routeUrl = `${apiUrl}/bloodCenter`;
+const serviceName = "BloodCenter";
 
 // get data
-router.get(routeUrl, (req, res, next) => handleRequest(
-    "get", req, res, next, serviceName, Controller
-));
+router.get(routeUrl, (req, res, next) =>
+  handleRequest("get", req, res, next, serviceName, Controller)
+);
 
 // create data
-router.post(routeUrl, (req, res, next) => handleRequest(
-    "post", req, res, next, serviceName, Controller
-));
+router.post(routeUrl, (req, res, next) =>
+  handleRequest("post", req, res, next, serviceName, Controller)
+);
 
 // update data
-router.put(routeUrl, (req, res, next) => handleRequest(
-    "put", req, res, next, serviceName, Controller
-));
+router.put(routeUrl, (req, res, next) =>
+  handleRequest("put", req, res, next, serviceName, Controller)
+);
 
 // delete data
-router.delete(routeUrl, (req, res, next) => handleRequest(
-    "delete", req, res, next, serviceName, Controller
-));
+router.delete(routeUrl, (req, res, next) =>
+  handleRequest("delete", req, res, next, serviceName, Controller)
+);
 
 module.exports = router;
