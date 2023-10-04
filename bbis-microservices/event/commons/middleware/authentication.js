643b38ffcf55672154d92389f9831a42a100c1f4
duwizerak <davy.uwizerak@gmail.com>
2023-10-04 18:31:12 +0000

    bbis-microservices/event/commons/middleware/authentication.js: update

--- bbis-microservices/event/commons/middleware/authentication.js
+++ bbis-microservices/event/commons/middleware/authentication.js
@@ -1,34 +1,84 @@
-/*
-aunthenticate gateway
-*/
-
 const jwt = require("jsonwebtoken");
+const appConfig = require("../config/app-config");
+const path = require("path");
+const fs = require("fs");
 
-// Public key for verification. This should ideally be stored in an environment variable.
-const PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
-...
------END PUBLIC KEY-----`;
+const publicKeyPath = path.join(__dirname, "../.keys", "public_key.pem");
+const PUBLIC_KEY = fs.readFileSync(publicKeyPath, "utf8");
+const KEY_PASSPHRASE = process.env.KEY_PASSPHRASE || "";
 
-const authenticateGateway = (req, res, next) => {
-  const bearerHeader = req.headers["authorization"];
+const authenticateGateway = async (req, res, next) => {
+  const isAuthUrl = appConfig.authUrls.includes(req.path.toLowerCase());
 
-  if (typeof bearerHeader !== "undefined") {
+  const bearerHeader = req.headers.Authorization || req.headers.authorization;
+  const hasBearerHeader =
+    (typeof bearerHeader !== "undefined" &&
+      !bearerHeader.includes("undefined") &&
+      bearerHeader !== "") ||
+    (req.method !== "GET" && !isAuthUrl);
+
+  // Check if the URL is in the whitelist
+  const isWhitelisted = appConfig.whitelistedUrls.includes(
+    req.path.toLowerCase()
+  );
+
+  console.log({
+    path: req.path.toLowerCase(),
+    bearerHeader,
+    hasBearerHeader,
+    isWhitelisted,
+  });
+
+  // Validate token if URL is not whitelisted or if a token is provided.
+  if (!isWhitelisted || hasBearerHeader) {
+    if (!hasBearerHeader) {
+      return res.status(403).json({ error: "Forbidden: No Token Provided" });
+    }
+
+    console.log({ bearerHeader });
+
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
 
-    jwt.verify(bearerToken, PUBLIC_KEY, { algorithms: ["RS256"] }, (err, payload) => {
-      if (err) {
-        return res.status(403).json({ error: "Forbidden: Invalid Token" });
-      }
-
-      req.apiGateway = payload;  // Add payload to request to be extended
-      next();
-    });
-  } else {
-    res.status(403).json({ error: "Forbidden: No Token Provided" });
+    try {
+      decoded = await jwt.verify(bearerToken, appConfig.secret);
+      req.body.decodedToken = decoded;
+    } catch (err) {
+      console.log(err);
+      return res.status(403).json({ error: "Forbidden: Invalid Token" });
+    }
   }
+
+  // Now decipher request body and query for all URLs, irrespective of whether they are whitelisted or not
+  try {
+    if (req.body && false) {
+      // disable for now
+      console.log({ body: req.body });
+      const body = await jwt.verify(req.body.hashedBody, {
+        key: PUBLIC_KEY,
+        passphrase: KEY_PASSPHRASE,
+      });
+      req.body = JSON.parse(body);
+      console.log({ body: req.body });
+    }
+
+    if (req.query && false) {
+      // disable for now
+      console.log({ query: req.query });
+      const query = await jwt.verify(req.query.hashedQuery, {
+        key: PUBLIC_KEY,
+        passphrase: KEY_PASSPHRASE,
+      });
+      req.query = JSON.parse(query);
+      console.log({ query: req.query });
+    }
+  } catch (err) {
+    return res.status(403).json({ error: "Forbidden: Invalid Key" });
+  }
+
+  next();
 };
 
 module.exports = {
-  authenticateGateway
+  authenticateGateway,
 };
