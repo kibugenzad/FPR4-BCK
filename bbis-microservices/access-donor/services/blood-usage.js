9efda0406e1d74b57317aa9b97d7ff5c4e0e02f9
duwizerak <davy.uwizerak@gmail.com>
2023-10-04 18:24:41 +0000

    bbis-microservices/access-donor/services/blood-usage.js: update

--- bbis-microservices/access-donor/services/blood-usage.js
+++ bbis-microservices/access-donor/services/blood-usage.js
@@ -45,7 +45,7 @@ class BloodUsage {
 
   static update(req) {
     const { id } = req.body;
-    return Model.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+    return Model.findByIdAndUpdate(id, req.body, { new: true });
   }
 
   static delete(req) {
