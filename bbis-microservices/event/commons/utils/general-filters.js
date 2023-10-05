5e27ea8cb8929d096eeb41427783e0e2782f1d7d
duwizerak <davy.uwizerak@gmail.com>
2023-10-05 08:47:50 +0000

    bbis-microservices/event/commons/utils/general-filters.js: update

--- bbis-microservices/event/commons/utils/general-filters.js
+++ bbis-microservices/event/commons/utils/general-filters.js
@@ -3,59 +3,59 @@ general filters
 */
 
 const generalFilters = {
-    filterDates: (query = {}, filters = {}) => {
-      const { startCreatedAt, endCreatedAt } = filters;
-      const updatedQuery = { ...query };
-  
-      if (startCreatedAt || endCreatedAt) {
-        updatedQuery.createdAt = { ...updatedQuery.createdAt };
-  
-        if (startCreatedAt) {
-          updatedQuery.createdAt.$gte = new Date(startCreatedAt);
-        }
-        if (endCreatedAt) {
-          updatedQuery.createdAt.$lte = new Date(endCreatedAt);
-        }
+  filterDates: (query = {}, filters = {}) => {
+    const { startCreatedAt, endCreatedAt } = filters;
+    const updatedQuery = { ...query };
+
+    if (startCreatedAt || endCreatedAt) {
+      updatedQuery.createdAt = { ...updatedQuery.createdAt };
+
+      if (startCreatedAt) {
+        updatedQuery.createdAt.$gte = new Date(startCreatedAt);
       }
-  
-      return updatedQuery;
-    },
-  
-    filterIds: (query = {}, filters = {}) => {
-      const { id, ids, startId } = filters;
-      const updatedQuery = { ...query };
-  
-      if (id && startId) {
-        // Handle scenario where both id and startId are present
-        // ...
-      } else {
-        if (id) {
-          updatedQuery._id = id;
-        }
-        if (startId) {
-          updatedQuery._id = { $lt: startId };
-        }
-      }
-  
-      if (ids) {
-        updatedQuery.ids = { $in: ids };
-      }
-  
-      return updatedQuery;
-    },
-  
-    processArrayQuery: (query, key, value) => {
-      if (value) {
-          query[key] = { "$in": value };
-      }
-    },
-    
-    processExactQuery: (query, key, value) => {
-      if (value) {
-          query[key] = value;
+      if (endCreatedAt) {
+        updatedQuery.createdAt.$lte = new Date(endCreatedAt);
       }
     }
-  };
-  
-  module.exports = generalFilters;
-  
+
+    return updatedQuery;
+  },
+
+  filterIds: (query = {}, filters = {}) => {
+    const { id, ids, startId } = filters;
+    const updatedQuery = { ...query };
+
+    if (id && startId) {
+      // Handle scenario where both id and startId are present
+      // ...
+    } else {
+      if (id) {
+        updatedQuery._id = id;
+      }
+      if (startId) {
+        updatedQuery._id = { $lt: startId };
+      }
+    }
+
+    if (ids) {
+      updatedQuery.ids = { $in: ids };
+    }
+
+    return updatedQuery;
+  },
+
+  processArrayQuery: (query, key, value) => {
+    if (value) {
+      query[key] = { $in: value };
+    }
+  },
+
+  processExactQuery: (query, key, value) => {
+    if (value !== null && value !== undefined) {
+      // allow false, 0, etc.
+      query[key] = value;
+    }
+  },
+};
+
+module.exports = generalFilters;
