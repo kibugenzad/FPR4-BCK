024391d2071267d5776fbbab5708ae4432f2c76a
duwizerak <davy.uwizerak@gmail.com>
2023-10-06 14:22:14 +0000

    bbis-microservices/event/controller/prsp-event.js: update

--- bbis-microservices/event/controller/prsp-event.js
+++ bbis-microservices/event/controller/prsp-event.js
@@ -6,37 +6,40 @@ the services
 
 const Services = require("../services");
 
-
 /*
 Controller
 */
 class bbisEvent {
-    static performAction(action, req, serviceName) {
-        // Validate the service name
-        const Service = Services[serviceName];
-        if (!Service) {
-            throw new Error(`Invalid service name: ${serviceName}`);
-        }
-
-        // Perform the action
-        return Service[action](req);
+  static performAction(action, req, serviceName) {
+    // Validate the service name
+    const Service = Services[serviceName];
+    if (!Service) {
+      throw new Error(`Invalid service name: ${serviceName}`);
     }
 
-    static get(req, serviceName) {
-        return this.performAction("get", req, serviceName);
-    }
+    // Perform the action
+    return Service[action](req);
+  }
 
-    static create(req, serviceName) {
-        return this.performAction("create", req, serviceName);
-    }
+  static get(req, serviceName) {
+    return this.performAction("get", req, serviceName);
+  }
 
-    static update(req, serviceName) {
-        return this.performAction("update", req, serviceName);
-    }
+  static last(req, serviceName) {
+    return this.performAction("last", req, serviceName);
+  }
 
-    static delete(req, serviceName) {
-        return this.performAction("delete", req, serviceName);
-    }
+  static create(req, serviceName) {
+    return this.performAction("create", req, serviceName);
+  }
+
+  static update(req, serviceName) {
+    return this.performAction("update", req, serviceName);
+  }
+
+  static delete(req, serviceName) {
+    return this.performAction("delete", req, serviceName);
+  }
 }
 
 module.exports = bbisEvent;
