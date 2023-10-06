9e570a64fd9434b1213695dba27d3d8e986f34ea
duwizerak <davy.uwizerak@gmail.com>
2023-10-06 14:33:59 +0000

    bbis-api-gateway/commons/config/app-config.js: update

--- bbis-api-gateway/commons/config/app-config.js
+++ bbis-api-gateway/commons/config/app-config.js
@@ -1,4 +1,6 @@
 module.exports = {
   apiPrefix: "/api",
   accessMicroserviceUrl: "http://localhost:3397/api",
+  eventMicroserviceUrl: "http://localhost:3387/api",
+  analyticsMicroserviceUrl: "http://localhost:3377/api",
 };
