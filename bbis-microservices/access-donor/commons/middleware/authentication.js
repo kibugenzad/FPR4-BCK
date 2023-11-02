5153f820c8d871e4592ae97b91677517f42e4015
duwizerak <davy.uwizerak@gmail.com>
2023-11-02 14:21:30 +0000

    bbis-microservices/access-donor/commons/middleware/authentication.js: update

--- bbis-microservices/access-donor/commons/middleware/authentication.js
+++ bbis-microservices/access-donor/commons/middleware/authentication.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const Controller = require("../../controller/bbis-access");
 const appConfig = require("../config/app-config");
 const path = require("path");
 const fs = require("fs");
@@ -6,21 +7,22 @@ const fs = require("fs");
 const publicKeyPath = path.join(__dirname, "../.keys", "public_key.pem");
 const PUBLIC_KEY = fs.readFileSync(publicKeyPath, "utf8");
 const KEY_PASSPHRASE = process.env.KEY_PASSPHRASE || "";
+const ACCESS_ROLE_SERVICE_NAME = "AccessRole";
+const GET_METHOD = "get";
 
 const authenticateGateway = async (req, res, next) => {
   const isAuthUrl = appConfig.authUrls.includes(req.path.toLowerCase());
+  // Check if the URL is in the whitelist
+  const isWhitelisted = appConfig.whitelistedUrls.includes(
+    req.path.toLowerCase()
+  );
 
   const bearerHeader = req.headers.Authorization || req.headers.authorization;
   const hasBearerHeader =
     (typeof bearerHeader !== "undefined" &&
       !bearerHeader.includes("undefined") &&
       bearerHeader !== "") ||
-    (req.method !== "GET" && !isAuthUrl);
-
-  // Check if the URL is in the whitelist
-  const isWhitelisted = appConfig.whitelistedUrls.includes(
-    req.path.toLowerCase()
-  );
+    (req.method !== "GET" && !isAuthUrl && !isWhitelisted);
 
   console.log({
     path: req.path.toLowerCase(),
@@ -41,13 +43,52 @@ const authenticateGateway = async (req, res, next) => {
     const bearerToken = bearer[1];
 
     try {
-      decoded = await jwt.verify(bearerToken, appConfig.secret);
-      const { accountType } = decoded;
+      const method = req.method;
+      const decoded = await jwt.verify(bearerToken, appConfig.secret);
+      let { accountType, accessRoleId } = decoded;
+
+      if (accessRoleId) {
+        var reqAccessRole = { body: { id: accessRoleId } };
+        var [accessRole] = await Controller[GET_METHOD](
+          reqAccessRole,
+          ACCESS_ROLE_SERVICE_NAME
+        );
+
+        console.log(accessRole);
+      }
+
+      if (
+        accessRole &&
+        accessRole.permissions &&
+        accessRole.permissions.operationPaths &&
+        accessRole.permissions.operationPaths[`_${method}`]
+      ) {
+        const operationPaths =
+          accessRole.permissions.operationPaths[`_${method}`];
+
+        if (!operationPaths.includes(req.path)) {
+          return res.status(403).json({ error: "Forbidden: Access Denided" });
+        }
+      } else if (appConfig.userRoles.includes(accountType)) {
+        if (!appConfig.userWhitelistedPaths.includes(req.path)) {
+          return res.status(403).json({ error: "Forbidden: Access Denided" });
+        }
+      } else if (!appConfig.superAdminRoles.includes(accountType)) {
+        return res.status(403).json({ error: "Forbidden: Access Denided" });
+      }
 
       if (
         !appConfig.adminRoles.includes(accountType) &&
         !appConfig.userWhitelistedPaths.includes(req.path)
       ) {
+        return res.status(403).json({ error: "Forbidden: Access Denided" });
+      }
+
+      if (
+        !appConfig.adminRoles.includes(accountType) &&
+        !appConfig.userWhitelistedPaths.includes(req.path)
+      ) {
+        console.log("here 5");
         return res.status(403).json({ error: "Forbidden: Access Denided" });
       }
 
