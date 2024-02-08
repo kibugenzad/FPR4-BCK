4370336049cd336c593cc3b362cdc3b373689172
duwizerak <davy.uwizerak@gmail.com>
2024-02-08 08:04:14 +0000

    bbis-microservices/access-donor/routers/pre-donation-assessment.js: update

---
+++ bbis-microservices/access-donor/routers/pre-donation-assessment.js
@@ -0,0 +1,35 @@
+/*
+handle departments requests
+per route
+*/
+const express = require("express");
+const router = express.Router();
+const { handleRequest } = require("../commons/utils/handler");
+const Controller = require("../controller/bbis-access");
+
+// Set main api URL and service name
+const apiUrl = "/access";
+const routeUrl = `${apiUrl}/preDonationAssessment`;
+const serviceName = "PreDonationAssessment";
+
+// get data
+router.get(routeUrl, (req, res, next) =>
+  handleRequest("get", req, res, next, serviceName, Controller)
+);
+
+// create data
+router.post(routeUrl, (req, res, next) =>
+  handleRequest("post", req, res, next, serviceName, Controller)
+);
+
+// update data
+router.put(routeUrl, (req, res, next) =>
+  handleRequest("put", req, res, next, serviceName, Controller)
+);
+
+// delete data
+router.delete(routeUrl, (req, res, next) =>
+  handleRequest("delete", req, res, next, serviceName, Controller)
+);
+
+module.exports = router;
