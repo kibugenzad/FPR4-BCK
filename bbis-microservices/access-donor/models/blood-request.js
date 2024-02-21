7b8b3c05671e325b5c523393768ec222e8d94af8
duwizerak <davy.uwizerak@gmail.com>
2024-02-21 06:39:23 +0000

    bbis-microservices/access-donor/models/blood-request.js: update

--- bbis-microservices/access-donor/models/blood-request.js
+++ bbis-microservices/access-donor/models/blood-request.js
@@ -1,6 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const QuestionSchema = new Schema({
+  duplicateId: {
+    type: String,
+  },
+  questionText: {
+    type: String,
+    required: [true, "Question text is required"],
+  },
+  options: [
+    {
+      type: String,
+    },
+  ],
+  answerType: {
+    // for example, 'text', 'multipleChoice', etc
+    type: String,
+    required: [true, "Answer type is required"],
+  },
+  answer: {
+    type: Schema.Types.Mixed,
+    // required: [true, "Answer is required"],
+  },
+  required: {
+    type: Boolean,
+    default: true,
+  },
+});
+
 // Packages sub-schema
 const packagesSchema = new Schema(
   {
@@ -45,13 +73,38 @@ const BloodRequestSchema = new Schema(
           type: Number,
           required: true,
         },
+        individualMaxResponseTime: {
+          type: Number,
+          validate: {
+            validator: function (value) {
+              return value > 0;
+            },
+            message: (props) =>
+              `${props.value} is not a valid response. It must be greater than 0.`,
+          },
+        },
+        action: {
+          type: String,
+          enum: ["Rejection", "Approval", "Revision"],
+        },
         approved: {
           type: Boolean,
           default: false,
         },
+        actionComment: {
+          type: String, // eg: comment on rejection or request of adjustment
+        },
+        questionnaireAction: [QuestionSchema],
+        processingTime: {
+          type: Number,
+        },
         approvedAt: Date,
+        actionAt: Date,
       },
     ],
+    questionnaireClarification: [QuestionSchema],
+    questionnaireRejection: [QuestionSchema],
+    questionnaireAnswer: [QuestionSchema],
     isActive: { type: Boolean, default: true },
   },
   {
