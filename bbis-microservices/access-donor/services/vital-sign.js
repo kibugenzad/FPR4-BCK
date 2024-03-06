17a2cbbde89a8408ae3c7717b8406c838cccd98c
teerenzo <renzahoemmanuel8@gmail.com>
2024-03-06 23:05:08 +0200

    adding vital-sign

---
+++ bbis-microservices/access-donor/services/vital-sign.js
@@ -0,0 +1,59 @@
+const Model = require("../models/vital-sign");
+const config = require("../commons/config/app-config");
+const {
+  filterDates,
+  filterIds,
+  processArrayQuery,
+  processExactQuery,
+} = require("../commons/utils/general-filters");
+
+class VitalSign {
+    static buildQuery(filters) {
+        let query = { }; 
+        query = filterDates(query, filters);
+        query = filterIds(query, filters);
+
+        for (const key in filters) {
+            console.log(key);
+            if(key!='decodedToken' &&  key!='limit' && key!='page' && key!='sortField' && key!='sortOrder'){
+                console.log(key);
+                query[key] = filters[key];
+            }
+          
+          }
+       
+        return query;
+    }
+    
+    static get(req) {
+        const { limit = config.limit, page, sortField = 'createdAt', sortOrder = '-1' } = req.body;
+        const query = this.buildQuery(req.body);
+    
+        const sort = { };
+        sort[sortField] = sortOrder;
+    
+        return Model.find(query)
+        .populate({ path: "equipment" })
+        .sort(sort)
+        .limit(limit)
+        .skip(page ? limit * (page - 1) : 0);
+    }
+    
+    static create(req) {
+        return Model.create(req.body);
+    }
+    
+    static update(req) {
+        const { id } = req.body;
+        return Model.findByIdAndUpdate(id, req.body, { new: true });
+    }
+    
+    static delete(req) {
+        const { id } = req.body;
+        return Model.remove({ _id: id });
+    }
+
+    }
+
+
+module.exports = VitalSign;
