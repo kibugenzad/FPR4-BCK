610e1dad035f7d6d4778a1220a68df1a59c0d901
teerenzo <renzahoemmanuel8@gmail.com>
2024-03-23 16:30:56 +0200

    adding database from env

--- bbis-microservices/access-donor/commons/config/db-config.js
+++ bbis-microservices/access-donor/commons/config/db-config.js
@@ -3,10 +3,6 @@
 mongoose = require("mongoose");
 
 const dbName = "bbis-access-donor";
-// mongoose.connect(
-//   `mongodb://huza:HzAmrt12Qpoly@polycluster0-shard-00-00-nnzhu.mongodb.net:27017,polycluster0-shard-00-01-nnzhu.mongodb.net:27017,polycluster0-shard-00-02-nnzhu.mongodb.net:27017/${dbName}?ssl=true&replicaSet=polycluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
-//   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
-// );
 mongoose.connect(
   `${process.env.MONGO_DB_URL}`,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
